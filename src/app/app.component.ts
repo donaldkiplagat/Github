0f9dc1ff10c9589fc5b7ac0e5545ea261c1842d6
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-09-22 12:45:23 +0300

    Basic Functionality of API added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Response, Http} from '@angular/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Github';
+  reposArray:any;
+
+
+  link = "https://api.github.com/users/";
+
+  access_token= "?access_token=3b535c05eaca64eff60db021cb296e3166d99cb1";
+  user=[];
+  repos= [];
+
+  constructor(private http:Http){}
+  searchGithub(search: HTMLInputElement){
+    let wholelink = this.link + search.value + this.access_token;
+    let wholelink2 = this.link + search.value +"/repos"+this.access_token;
+    this.http.get(wholelink).subscribe((res:Response)=>{
+      this.user = res.json();
+      console.log(this.user);
+    })
+    this.http.get(wholelink2).subscribe((res2:Response)=>{
+      this.repos = res2.json();
+      console.log(this.repos);
+    })
+
+  }
+
+
+  ngOninit(){
+  //   this.http.get("https://api.github.com/users/donaldkiplagat?access_token=3b535c05eaca64eff60db021cb296e3166d99cb1").subscribe((res:Response)=>{
+  //   console.log(res.json());
+  // })
+  }
 }
