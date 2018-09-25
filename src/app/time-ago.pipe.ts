952e13bcda0aaf70f67515de30d3ca0f4b6129ae
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-09-25 11:34:30 +0300

    Pipe created

--- src/app/time-ago.pipe.ts
+++ src/app/time-ago.pipe.ts
@@ -5,25 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class TimeAgoPipe implements PipeTransform {
 
-  transform(value: any): number {
-    let today:Date = new Date(); //get current date and time
-    let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
-
-    var dateDifference =Math.abs(value-todayWithNoTime)// returns value in milliseconds
-    // const secondsInADay= 86400; //60 seconds*60 minutes in an hour *24 hours
-    //
-    // var dateDifferenceSeconds=dateDifference*0.001; //converts to seconds
-
-    // var dateCounter = dateDifferenceSeconds/secondsInADay;
-    var dateCounter = todayWithNoTime;
-
-
-    if (dateCounter >= 1){
-        return todayWithNoTime;
-
-    }else{
-        return todayWithNoTime;
-
-    }
-  }
+    transform(value: any): number {
+       let today:Date = new Date(); //get current date and time
+       let todayWithNoTime:any = new Date(today.getFullYear(),today.getMonth(),today.getDate())
+       console.log(todayWithNoTime);
+       return todayWithNoTime;
+     }
 }
