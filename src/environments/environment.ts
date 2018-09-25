b107765ccc32dde901e0b9182c2b6d686616459b
Donald Kiplagat <donald.k.kiplagat@gmail.com>
2018-09-25 09:43:31 +0300

    New Access Token

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,9 +5,9 @@
 export const environment = {
   production: false,
   apiUrl:"https://api.github.com",
-  apiKey: "8575a848b0ff725a54739e8a5c9f3a83359dd0f4",
-  access_token:"?access_token=8575a848b0ff725a54739e8a5c9f3a83359dd0f4",
-  access_token2: "?access_token=8575a848b0ff725a54739e8a5c9f3a83359dd0f4&q=",
+  apiKey: "d0302a22650bcac4a78f2beb9a614621a20ce3ac",
+  access_token:"?access_token=d0302a22650bcac4a78f2beb9a614621a20ce3ac",
+  access_token2: "?access_token=d0302a22650bcac4a78f2beb9a614621a20ce3ac&q=",
   link : "https://api.github.com/users/",
   link2 : "https://api.github.com/search/",
 
